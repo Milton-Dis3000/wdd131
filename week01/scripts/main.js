95fe93441d435407a654e9763f9ce0c91ce25a07
Milton-Dis3000 <msigchaquila@byupathway.edu>
2025-09-06 22:56:58 -0500

    Introduction to JavaScript

---
+++ week01/scripts/main.js
@@ -0,0 +1,5 @@
+// JavaScript to demonstrate functionality
+document.addEventListener("DOMContentLoaded", function() {
+    const mainParagraph = document.querySelector(".content p");
+    mainParagraph.textContent = "Hello! JavaScript is successfully running on this page.";
+});
